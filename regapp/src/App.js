f4da28c32265204a9e1073c4a1ef2315b421f35c
Arnab <arnab1090.be22@chitkarauniversity.edu.in>
2024-08-15 17:51:28 +0530

    h

--- regapp/src/App.js
+++ regapp/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const App=()=>{
+const App = () => {
   return (
-  <section className="bg-emerald-500 flex items-center justify-center h-screen">App </section>
-  )
+    <section className="bg-emerald-500 flex items-center justify-center h-screen">
+      App
+    </section>
+  );
 };
-//bg-emerald-500 flex items-center justify-center h-screen
 
 export default App;
