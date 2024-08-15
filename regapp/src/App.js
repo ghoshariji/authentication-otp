da9fc85756004b253d8fd2ebdd932cbcab74076d
Arnab <arnab1090.be22@chitkarauniversity.edu.in>
2024-08-15 17:16:02 +0530

    hi

---
+++ regapp/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App=()=>{
+  return (
+  <section className="bg-emerald-500 flex items-center justify-center h-screen">App </section>
+  )
+};
+//bg-emerald-500 flex items-center justify-center h-screen
+
+export default App;
---
+++ regapp/src/firbase.config.js
@@ -0,0 +1,21 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4yOorRggow0OWXRCTGHPWK63L2cu-Wqo",
+  authDomain: "otp-project-aa69c.firebaseapp.com",
+  projectId: "otp-project-aa69c",
+  storageBucket: "otp-project-aa69c.appspot.com",
+  messagingSenderId: "43625493513",
+  appId: "1:43625493513:web:635a27cef164c1826eb4d8",
+  measurementId: "G-NT1RYTKLTE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
